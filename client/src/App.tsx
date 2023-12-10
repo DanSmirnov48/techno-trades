4cb5c0efa75451f8d2fc9c4471952b22f62b1c71
Dan <daniel.smirnov17@gmail.com>
2023-12-10 20:10:50 +0000

    client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-400 font-jost text-3xl">This is a setup</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
