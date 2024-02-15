16e04f4782a713dfd4b7842b5055cf07d2083c62
Dan <daniel.smirnov17@gmail.com>
2024-02-15 16:16:23 +0000

    change: (Clinet) Use Heroku app link

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://techno-trades-b8640249eefb.herokuapp.com',
         changeOrigin: true,
         secure: false,
       }
