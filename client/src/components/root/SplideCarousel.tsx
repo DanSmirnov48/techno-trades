81a527936c6f51da2ca0374ce49bd4715988dece
Dan <daniel.smirnov17@gmail.com>
2024-02-15 15:42:43 +0000

    added: (Client) added media files and global styles

--- client/src/components/root/SplideCarousel.tsx
+++ client/src/components/root/SplideCarousel.tsx
@@ -1,12 +1,12 @@
 //@ts-ignore
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
-import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
-import "@splidejs/splide/dist/css/splide.min.css";
-import { useGetProducts } from "@/lib/react-query/queries";
-import { Product } from "@/types";
 import { calculateDiscountPercentage, cn, formatPrice } from "@/lib/utils";
-import { Link } from "react-router-dom";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+import { useGetProducts } from "@/lib/react-query/queries";
+import "@splidejs/splide/dist/css/splide.min.css";
 import { buttonVariants } from "../ui/button";
+import { Link } from "react-router-dom";
+import { Product } from "@/types";
 
 const SplideCarousel = () => {
     const { data, isPending: isProductLoading } = useGetProducts();
@@ -16,75 +16,72 @@ const SplideCarousel = () => {
         data.data.products.filter((product: Product) => product.isDiscounted === true);
 
     return (
-        <div className="flex items-center justify-center h-[25vh]">
-            <Splide
-                hasTrack={false}
-                options={{
-                    type: "loop",
-                    drag: "free",
-                    arrows: true,
-                    pagination: true,
-                    perPage: 4,
-                    width: "100%",
-                    gap: "25px",
-                    breakpoints: {
-                        767: {
-                            width: "100%",
-                            perPage: 2,
-                            padding: { right: "5rem" },
-                        },
-                        1000: {
-                            perPage: 1,
-                            padding: { right: "4rem" },
-                        },
-                        1200: {
-                            padding: { right: "8rem" },
-                        },
+        <Splide
+            hasTrack={false}
+            options={{
+                type: "loop",
+                drag: "free",
+                arrows: true,
+                pagination: true,
+                perPage: 4,
+                width: "100%",
+                gap: "25px",
+                breakpoints: {
+                    767: {
+                        width: "100%",
+                        perPage: 2,
+                        padding: { right: "5rem" },
                     },
-                    autoScroll: {
-                        pauseOnHover: true,
-                        pauseOnFocus: true,
-                        rewind: false,
-                        speed: 3
-                    }
-                }}
-                className={""}
-                extensions={{ AutoScroll }}
-            >
-                <div className="p-10">
-                    <SplideTrack>
-                        {!isProductLoading && filteredProducts.map((product: Product) => (
-                            <SplideSlide key={product._id} className="splide__slide relative bg-white dark:bg-dark-4 border-2 rounded-xl font-jost">
-                                <span className="absolute top-0 left-0 px-6 py-3 rounded-tl-xl rounded-br-xl bg-purple-100 text-lg font-bold text-purple-800 ring-1 ring-inset ring-purple-600/30">
-                                    {calculateDiscountPercentage({ normalPrice: product.price, discountedPrice: product.discountedPrice })}%
-                                </span>
-                                <div className="flex w-full flex-col self-center mt-10 gap-10">
-                                    <div className="select-none flex items-center justify-center bg-white m-auto p-5 rounded-lg mt-5">
-                                        <img className="w-44 h-44 object-scale-down" src={product.image[0].url} alt="post" />
-                                    </div>
-                                    <div className="flex flex-col px-5 pb-5 gap-4">
-                                        <h5 className="text-2xl font-medium tracking-tight text-dark-3 dark:text-white/80">{product.name}</h5>
-                                        <div className="mb-5 flex items-center justify-between text-dark-3 dark:text-white/80">
-                                            <span className="text-2xl font-semibold">
-                                                <span>{product && formatPrice(product.discountedPrice!, { currency: "GBP" })}</span>
-                                                <span className="ml-3 text-base font-normal line-through">
-                                                    {product && formatPrice(product.price, { currency: "GBP" })}
-                                                </span>
-                                            </span>
-                                        </div>
-                                        <Link to={`/products/${product.slug}`}
-                                            className={cn(buttonVariants(), "w-full bg-dark-4 dark:bg-dark-2 py-6 text-2xl dark:text-white/80")}
-                                        >
-                                            View Rroduct
-                                        </Link>
-                                    </div>
+                    1000: {
+                        perPage: 1,
+                        padding: { right: "4rem" },
+                    },
+                    1200: {
+                        padding: { right: "8rem" },
+                    },
+                },
+                autoScroll: {
+                    pauseOnHover: true,
+                    pauseOnFocus: true,
+                    rewind: false,
+                    speed: 3
+                }
+            }}
+            className={""}
+            extensions={{ AutoScroll }}
+        >
+            <div className="p-10">
+                <SplideTrack>
+                    {!isProductLoading && filteredProducts.map((product: Product) => (
+                        <SplideSlide key={product._id} className="splide__slide relative bg-white dark:bg-dark-4 border-2 rounded-xl font-jost">
+                            <span className="absolute top-0 left-0 px-6 py-3 rounded-tl-xl rounded-br-xl bg-purple-100 text-lg font-bold text-purple-800 ring-1 ring-inset ring-purple-600/30">
+                                {calculateDiscountPercentage({ normalPrice: product.price, discountedPrice: product.discountedPrice })}%
+                            </span>
+                            <div className="flex w-full flex-col self-center mt-10 gap-10">
+                                <div className="select-none flex items-center justify-center bg-white m-auto p-5 rounded-lg mt-5">
+                                    <img className="w-44 h-44 object-scale-down" src={product.image[0].url} alt="post" />
                                 </div>
-                            </SplideSlide>
-                        ))}
-                    </SplideTrack>
-                </div>
-            </Splide>
-
+                                <div className="flex flex-col px-5 pb-5 gap-4">
+                                    <h5 className="text-2xl font-medium tracking-tight text-dark-3 dark:text-white/80">{product.name}</h5>
+                                    <div className="mb-5 flex items-center justify-between text-dark-3 dark:text-white/80">
+                                        <span className="text-2xl font-semibold">
+                                            <span>{product && formatPrice(product.discountedPrice!, { currency: "GBP" })}</span>
+                                            <span className="ml-3 text-base font-normal line-through">
+                                                {product && formatPrice(product.price, { currency: "GBP" })}
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <Link to={`/products/${product.slug}`}
+                                        className={cn(buttonVariants(), "w-full bg-dark-4 dark:bg-dark-2 py-6 text-2xl dark:text-white/80")}
+                                    >
+                                        View Rroduct
+                                    </Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                    ))}
+                </SplideTrack>
+            </div>
             <style>
                 {`
                 .splide__arrow {
@@ -101,8 +98,7 @@ const SplideCarousel = () => {
                 }
                 `}
             </style>
-
-        </div>
+        </Splide>
     );
 }
 
