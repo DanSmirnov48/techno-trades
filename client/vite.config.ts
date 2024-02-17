d8a4de227eea61c325b358c55699d82d3e476620
Dan <daniel.smirnov17@gmail.com>
2024-02-17 21:33:28 +0000

    added: (Client) Loading proxy url from .env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,30 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const url =
+    process.env.VITE_ENVIRONMENT === "development"
+      ? process.env.VITE_LOCAL_SERVER_URL
+      : process.env.VITE_PROD_SERVER_URL;
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://techno-trades-b8640249eefb.herokuapp.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: url,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};
