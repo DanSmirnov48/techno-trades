fcfe41e52d1d71139555a2e076ede982a1b34310
Dan <daniel.smirnov17@gmail.com>
2024-03-14 13:48:09 +0000

    change: (Client) Better error handling for Magic SignIn

--- client/src/_auth/forms/MagicLinkSignIn.tsx
+++ client/src/_auth/forms/MagicLinkSignIn.tsx
@@ -9,10 +9,11 @@ import {
 } from "@/components/ui/form";
 import * as z from "zod";
 import { toast } from "sonner";
-import { Forward } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { AlertCircle, Forward } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMagicLinkSignIn } from "@/lib/react-query/queries";
 
@@ -29,6 +30,7 @@ const MagicLinkSignInValidation = z.object({
 
 export default function MagicLinkSignIn() {
 
+    const [error, setError] = useState<string | undefined>();
     const { mutateAsync: magicLinkSignIn } = useMagicLinkSignIn()
 
     const form = useForm<z.infer<typeof MagicLinkSignInValidation>>({
@@ -38,58 +40,78 @@ export default function MagicLinkSignIn() {
         },
     });
 
+    const handleNewEmailChange = (newEmail: string) => {
+        form.clearErrors("email")
+        form.setValue('email', newEmail);
+        setError(undefined)
+    };
+
     const handleSignin = async (user: z.infer<typeof MagicLinkSignInValidation>) => {
         const res: MagicLinkResponse = await magicLinkSignIn({ email: user.email })
-        console.log(res)
         if (res.status === 200 && res.data.status === "success") {
             form.reset()
             toast.info(res.data.message)
+        } else if (res.error.error === 'User not found') {
+            form.setFocus("email")
+            setError(res.error.error)
         } else {
-            toast.info('Something went wrong. Fuck you!!')
+            toast.info('Something went wrong.')
         }
     };
 
     return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(handleSignin)}
-                className="flex flex-col items-center sm:flex-row my-5"
-            >
-                <div className="flex flex-row items-center mb-4 sm:mb-0 w-full sm:mr-2">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem className="w-full">
-                                <FormLabel className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
-                                    Email Address
-                                </FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="email"
-                                        placeholder="Email"
-                                        className="block w-full px-4 py-2 h-12"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    We will email you a{" "}
-                                    <span className="italic font-medium">Magic Link</span> to Sign
-                                    in to your account!
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+        <>
+            {error &&
+                <div className="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+                    <AlertCircle className="w-6 h-6 mr-2" />
+                    <span className="sr-only">Info</span>
+                    <div className="text-base">
+                        <span className="font-medium">{error}</span>
+                    </div>
                 </div>
-                <Button
-                    type="submit"
-                    variant={"outline"}
-                    className="flex-shrink-0 h-12"
+            }
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(handleSignin)}
+                    className="flex flex-col items-center sm:flex-row my-5"
                 >
-                    <Forward className="h-6 w-6 text-dark-4" />
-                </Button>
-            </form>
-        </Form>
+                    <div className="flex flex-row items-center mb-4 sm:mb-0 w-full sm:mr-2">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem className="w-full">
+                                    <FormLabel className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                                        Email Address
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            placeholder="Email"
+                                            className="block w-full px-4 py-2 h-12"
+                                            {...field}
+                                            onChange={(e) => handleNewEmailChange(e.target.value)}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        We will email you a{" "}
+                                        <span className="italic font-medium">Magic Link</span> to Sign
+                                        in to your account!
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        variant={"outline"}
+                        className="flex-shrink-0 h-12"
+                    >
+                        <Forward className="h-6 w-6 text-dark-4" />
+                    </Button>
+                </form>
+            </Form>
+        </>
     );
 }
