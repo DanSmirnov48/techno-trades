1326b9b475aadbb4470e3aed3b99d16da522d801
Dan <daniel.smirnov17@gmail.com>
2024-11-08 21:43:27 +0000

    changed: (Server) Moved email templates away from utils folder into its own `templates` folder in the root

---
+++ server/src/templates/index.ts
@@ -0,0 +1,4 @@
+export { default as EmailChangeVerification } from './EmailChangeVerification'
+export { default as ForgotPasswordVerification } from './ForgotPasswordVerification'
+export { default as MagicLinkSignIn } from './MagicLinkSignIn'
+export { default as VerifyEmail } from './VerifyEmail'
