01addd47a1d3feb013b51b4d64743409f2d41a27
Dan <daniel.smirnov17@gmail.com>
2024-12-04 23:27:31 +0000

    updated: (Server) `Prodcutc` model

--- server/src/models/products.ts
+++ server/src/models/products.ts
@@ -1,55 +1,53 @@
-import mongoose, { Model, Query, Schema, Types } from 'mongoose';
+import mongoose, { model, Model, Query, Schema, Types } from 'mongoose';
 import slugify from 'slugify'
+
+enum RATING_CHOICES {
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+}
 
 interface IReview {
     name: string;
-    rating: number;
+    rating: RATING_CHOICES
     title: string;
     comment: string;
     user: Types.ObjectId;
 }
 
-export interface IProduct extends Document {
+interface IProduct extends Document {
     user: Types.ObjectId;
-    slug: string;
     name: string;
+    slug: string;
+    description?: string;
+    price: number;
+    isDiscounted: boolean;
+    discountedPrice?: number;
+    category?: string;
+    brand?: string;
+    countInStock: number;
     image: Array<{
         key: string;
         name: string;
         url: string;
     }>;
-    brand?: string;
-    category?: string;
-    description?: string;
     reviews: IReview[];
-    rating: number;
-    numReviews: number;
-    price: number;
-    countInStock: number;
-    isDiscounted: boolean;
-    discountedPrice?: number;
-    isArchived?: boolean;
-    [key: string]: any;
-}
-
-interface IProductMethods {
-    getArchivedProducts(): Promise<IProduct[]>;
-    getAllProducts(): Promise<IProduct[]>;
+    reviewsCount: number;
+    avgRating: number;
 }
 
 const reviewSchema = new Schema<IReview>({
-    name: { type: String, required: true },
-    rating: { type: Number, required: true },
     title: { type: String, required: true },
     comment: { type: String, required: true },
+    rating: RATING_CHOICES,
     user: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: 'User',
     },
 }, { timestamps: true });
-
-type ProductModel = Model<IProduct> & IProductMethods;
 
 const ProductSchema = new Schema<IProduct>({
     user: {
@@ -57,8 +55,15 @@ const ProductSchema = new Schema<IProduct>({
         required: true,
         ref: 'User',
     },
-    slug: String,
-    name: { type: String, required: true },
+    name: { type: String, required: true, maxlength: 500 },
+    slug: { type: String },
+    description: { type: String },
+    price: { type: Number, required: true, default: 0 },
+    isDiscounted: { type: Boolean, required: true, default: false },
+    discountedPrice: { type: Number },
+    category: { type: String },
+    brand: { type: String },
+    countInStock: { type: Number, required: true, default: 0 },
     image: [
         {
             key: { type: String, required: true },
@@ -66,60 +71,30 @@ const ProductSchema = new Schema<IProduct>({
             url: { type: String, required: true },
         },
     ],
-    brand: String,
-    category: String,
-    description: String,
     reviews: [reviewSchema],
-    rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, default: 0 },
-    price: { type: Number, required: true, default: 0 },
-    countInStock: { type: Number, required: true, default: 0 },
-    isDiscounted: { type: Boolean, required: true, default: false },
-    discountedPrice: { type: Number },
-    isArchived: { type: Boolean, required: true, default: false },
 }, { timestamps: true });
 
-// ProductSchema.pre(/^find/, function (this: Query<IProduct[], IProduct>, next) {
-//     this.find({ isArchived: { $ne: true } });
-//     next();
-// });
+ProductSchema.pre('save', async function (next) {
+    try {
+        if (this.isModified('name') || this.isNew) {
+            const newSlug = slugify(this.name, { lower: true, strict: true });
+            this.slug = newSlug;
+        }
+        next();
+    } catch (error: any) {
+        next(error)
+    }
+});
 
-ProductSchema.statics.getArchivedProducts = async function () {
-    return this.find({ isArchived: true });
-};
+ProductSchema.virtual('reviewsCount').get(function (this: IProduct) {
+    return this.reviews.length;
+});
 
-ProductSchema.statics.getAllProducts = async function () {
-    return this.find({ isArchived: { $ne: true } });
-};
+ProductSchema.virtual('avgRating').get(function (this: IProduct) {
+    const reviews = this.reviews
+    return reviews.reduce((sum, item) => sum + (item["rating"] || 0), 0) / reviews.length || 0;
+});
 
-export const ProductModel = mongoose.model<IProduct, ProductModel>('Product', ProductSchema);
+const Product = model<IProduct>('Product', ProductSchema);
 
-export const GetProduct = () => ProductModel.find()
-
-export const GetProductById = (id: any) => ProductModel.findById({ id })
-
-export const GetproductByName = (name: string) => ProductModel.findOne({ name })
-
-export const GetproductBySlug = (slug: string) => ProductModel.findOne({ slug })
-
-// export const CreateProduct = async (values: Record<string, any>) =>
-//     new ProductModel(values).save().then((product) => product.toObject())
-
-export const DeleteProductById = (id: string) =>
-    ProductModel.findByIdAndDelete(id)
-
-export const ArchiveProductById = (id: string) =>
-    ProductModel.findByIdAndUpdate(id, { $set: { isArchived: true } });
-
-export const UpdateProductById = (id: string, values: Record<string, typeof ProductModel>) =>
-    ProductModel.findByIdAndUpdate(id, values)
-
-export const UpdateProductStockById = (_id: string, countInStock: number) =>
-    ProductModel.findByIdAndUpdate(_id, { $set: { countInStock } }, { new: true });
-
-export const CreateProduct = async (values: Record<string, any>) => {
-    const product = new ProductModel(values);
-    product.slug = slugify(product.name, { lower: true });
-    const savedProduct = await product.save();
-    return savedProduct.toObject();
-};
+export { IProduct, Product, IReview, RATING_CHOICES }
